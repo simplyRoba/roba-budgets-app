db8e753afd05d0b7b6655c5b001d6dd43e9a257a
Axel Rojas Bachem <git@simplyroba.de>
2024-08-11 16:35:40 +0200

    feat: add overview and settings route (#5)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('roba-budgets-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, roba-budgets-app',
-    );
-  });
 });
---
+++ src/app/settings/settings.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { SettingsComponent } from './settings.component';
+
+const settingsRoutes: Routes = [{ path: '', component: SettingsComponent }];
+
+export default settingsRoutes;
