d85173dcf47a5cf8f68247142ea21d469569aa3c
Axel Rojas Bachem <axel.rojasbachem@rewe-group.com>
2024-08-30 10:13:45 +0200

    remove footer add buttons to header

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -4,11 +4,13 @@ import { Observable, of, startWith, switchMap } from 'rxjs';
 import { Income } from '../shared/income.model';
 import { Summary } from '../shared/summary.model';
 import { AsyncPipe, CurrencyPipe, DatePipe } from '@angular/common';
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faChevronLeft, faChevronRight, faHome, faPlus} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [AsyncPipe, CurrencyPipe, DatePipe],
+  imports: [AsyncPipe, CurrencyPipe, DatePipe, FaIconComponent],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss',
 })
@@ -35,4 +37,9 @@ export class SummaryComponent {
       ),
     );
   }
+
+  protected readonly faHome = faHome;
+  protected readonly faChevronLeft = faChevronLeft;
+  protected readonly faPlus = faPlus;
+  protected readonly faChevronRight = faChevronRight;
 }
