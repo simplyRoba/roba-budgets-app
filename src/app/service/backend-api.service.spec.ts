4e1ee9f0c91206d979401205f0bda316e53f94d8
Axel Rojas Bachem <axel.rojasbachem@rewe-group.com>
2024-09-01 13:29:37 +0200

    test: fix test

--- src/app/service/backend-api.service.spec.ts
+++ src/app/service/backend-api.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BackendApiService } from './backend-api.service';
+import {provideHttpClientTesting} from "@angular/common/http/testing";
+import {provideHttpClient} from "@angular/common/http";
 
 describe('BackendApiService', () => {
   let service: BackendApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(BackendApiService);
   });
 
--- src/app/summary/summary.component.spec.ts
+++ src/app/summary/summary.component.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SummaryComponent } from './summary.component';
+import {BackendApiService} from "../service/backend-api.service";
+import {Observable, of} from "rxjs";
+import {Income} from "../shared/income.model";
 
 describe('SummaryComponent', () => {
   let component: SummaryComponent;
   let fixture: ComponentFixture<SummaryComponent>;
 
   beforeEach(async () => {
+    const backendApiService: Partial<BackendApiService> = {
+      loadSummary: (year, month) => of({ month: month, year: year, totalIncomeInCents: 0, totalFixExpensesInCents: 0, totalFlexExpensesInCents: 0 }),
+      loadIncomeList: () => of([]),
+    };
+
     await TestBed.configureTestingModule({
       imports: [SummaryComponent],
+      providers: [
+        {
+          provide: BackendApiService,
+          useValue: backendApiService,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SummaryComponent);
