2acc247a8a797c88eb51cbdf85fb66db8328347e
Axel Rojas Bachem <git@simplyroba.de>
2024-09-01 17:28:31 +0200

    feat: navigation (#27)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideRouter(routes)],
     }).compileComponents();
   });
 
@@ -12,11 +15,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'roba-budgets-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('roba-budgets-app');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import {
+  faGear,
+  faHome,
+  faMoneyBill,
+  faMoneyBillTransfer,
+  faMoneyBillWave,
+  faSackDollar,
+  faWallet,
+} from '@fortawesome/free-solid-svg-icons';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FaIconComponent, RouterLink, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'roba-budgets-app';
+  navMenuOpen = false;
+  protected readonly faHome = faHome;
+  protected readonly faSackDollar = faSackDollar;
+  protected readonly faWallet = faWallet;
+  protected readonly faMoneyBillTransfer = faMoneyBillTransfer;
+  protected readonly faGear = faGear;
+  protected readonly faMoneyBill = faMoneyBill;
+  protected readonly faMoneyBillWave = faMoneyBillWave;
 }
