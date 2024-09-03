0cb146632cf45ca5cd2f9ab5548102ce7499f077
Axel Rojas Bachem <git@simplyroba.de>
2024-09-03 14:21:15 +0200

    feat: add paging for summary (#28)

--- src/app/summary/summary.routes.ts
+++ src/app/summary/summary.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { SummaryComponent } from './summary.component';
 
-const summaryRoutes: Routes = [{ path: '', component: SummaryComponent }];
+const summaryRoutes: Routes = [
+  { path: '', component: SummaryComponent },
+  { path: ':year/:month', component: SummaryComponent },
+];
 
 export default summaryRoutes;
